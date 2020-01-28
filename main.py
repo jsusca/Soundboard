f374f40da632cc11dd0fc2d74d9d2e5a93f35517
JSusca <jsusca25@gmail.com>
2020-01-28 16:02:31 -0500

    Add buttons, adjust sizing, create sound directory

--- main.py
+++ main.py
@@ -1,22 +1,44 @@
-import tkinter
+from tkinter import *
+from winsound import *
+
+
+def play():
+    PlaySound("SystemExit", SND_ASYNC)
 
 
 # Create blank window
-window = tkinter.Tk()
-
+root = Tk()
 # Rename window
-window.title("Soundboard")
+root.title("Soundboard")
 
-label = tkinter.Label(window, text="Welcome to the soundboard!")
+label = Label(root, text="Welcome to the soundboard!").grid(row=0, column=0, columnspan=4, pady=10)
 
-btn1 = tkinter.Button(text="Button1", fg="red").grid(row=0, column=0)
-btn2 = tkinter.Button(text="Button2", fg="green").grid(row=0, column=1)
-btn3 = tkinter.Button(text="Button3", fg="purple").grid(row=0, column=2)
-btn4 = tkinter.Button(text="Button4", fg="red").grid(row=1, column=0)
-btn5 = tkinter.Button(text="Button5", fg="green").grid(row=1, column=1)
-btn6 = tkinter.Button(text="Button6", fg="purple").grid(row=1, column=2)
-btn7 = tkinter.Button(text="Button7", fg="red").grid(row=2, column=0)
-btn8 = tkinter.Button(text="Button8", fg="green").grid(row=2, column=1)
-btn9 = tkinter.Button(text="Button9", fg="purple").grid(row=2, column=2)
+# Create buttons
+btn1 = Button(height=5, width=15, command=play)
+btn2 = Button(height=5, width=15)
+btn3 = Button(height=5, width=15)
+btn4 = Button(height=5, width=15)
+btn5 = Button(height=5, width=15)
+btn6 = Button(height=5, width=15)
+btn7 = Button(height=5, width=15)
+btn8 = Button(height=5, width=15)
+btn9 = Button(height=5, width=15)
+btn10 = Button(height=5, width=15)
+btn11 = Button(height=5, width=15)
+btn12 = Button(height=5, width=15)
 
-window.mainloop()
+# Place buttons on root window
+btn1.grid(row=1, column=0, padx=2, pady=2)
+btn2.grid(row=1, column=1, padx=2, pady=2)
+btn3.grid(row=1, column=2, padx=2, pady=2)
+btn4.grid(row=1, column=3, padx=2, pady=2)
+btn5.grid(row=2, column=0, padx=2, pady=2)
+btn6.grid(row=2, column=1, padx=2, pady=2)
+btn7.grid(row=2, column=2, padx=2, pady=2)
+btn8.grid(row=2, column=3, padx=2, pady=2)
+btn9.grid(row=3, column=0, padx=2, pady=2)
+btn10.grid(row=3, column=1, padx=2, pady=2)
+btn11.grid(row=3, column=2, padx=2, pady=2)
+btn12.grid(row=3, column=3, padx=2, pady=2)
+
+root.mainloop()
